f8917b25fdebea36efd6dd6a7466d05a949f2201
kiannaquines <kjgnaquines@gmail.com>
2025-11-27 00:27:01 +0800

    feat: Implement JWT authorization and enhance API initialization

--- core/__init__.py
+++ core/__init__.py
@@ -9,33 +9,53 @@ from .application.task.task_service import TaskService
 from .application.user.user_service import UserService
 from flask_restx import Api
 
+authorizations = {
+    "Bearer Auth": {
+        "type": "apiKey",
+        "in": "header",
+        "name": "Authorization",
+        "description": 'JWT Authorization header using the Bearer scheme. Example: "Authorization: Bearer {token}"',
+    }
+}
+
 api = Api(
     version="1.0.0",
     title="Task Management API",
     description="A python flask task management API",
     contact="Support Team",
-    contact_email="kjgnaquines@gmail.com"
+    contact_email="kjgnaquines@gmail.com",
+    security="Bearer Auth",
+    authorizations=authorizations,
 )
 
 def create_app(config=DevelopmentConfig):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(config or DevelopmentConfig)
+
+    # Initialize extensions
     db.init_app(app)
     migrate.init_app(app, db)
     jwt.init_app(app)
     api.init_app(app)
     limiter.init_app(app)
+    # Dependency Injection
     task_repo = bind_task_repository()
     user_repo = bind_user_repository()
+
+    # Create service instances
     task_service = TaskService(task_repo)
     user_service = UserService(user_repo)
+
+    # Attach services to app for global access
     app.task_service = task_service
     app.user_service = user_service
 
+    # Import models to register with SQLAlchemy
     from .domain.task import Task
     from .domain.user import User
 
-    api.add_namespace(main_ns,path="/")
+    # Register namespaces
+    api.add_namespace(main_ns, path="/")
     api.add_namespace(auth_ns, path="/auth")
     api.add_namespace(task_ns, path="/tasks")
 
